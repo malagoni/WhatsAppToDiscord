24bffe55409c37999823d9e076cb794060fc1ffe
arespawn <sniperman_koukou@hotmail.com>
2025-07-31 13:02:24 +0300

    Update index.js
    Bump version

--- src/index.js
+++ src/index.js
@@ -12,7 +12,7 @@ const storage = require('./storage.js');
 const whatsappHandler =  require('./whatsappHandler.js');
 
 (async () => {
-  const version = 'v0.10.30';
+  const version = 'v1.1.7';
   const streams = [
     { stream: pino.destination('logs.txt') },
     { stream: pretty({ colorize: true }) },
